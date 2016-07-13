defe77ede2f35614e07bb84a99edf4e8ef670111
Nicolas Widart <n.widart@gmail.com>
2016-07-13 22:49:53 +0200

    Testing the TagManager class

---
+++ Tests/BaseTestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modules\Tag\Tests;
+
+use Illuminate\View\ViewServiceProvider;
+use Modules\Core\Providers\CoreServiceProvider;
+use Modules\Tag\Providers\TagServiceProvider;
+use Nwidart\Modules\LaravelModulesServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+class BaseTestCase extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            ViewServiceProvider::class,
+            LaravelModulesServiceProvider::class,
+            CoreServiceProvider::class,
+            TagServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['path.base'] = __DIR__ . '/..';
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', array(
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ));
+        $app['config']->set('translatable.locales', ['en', 'fr']);
+    }
+}
---
+++ Tests/TagManagerRepositoryTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Modules\Tag\Tests;
+
+use Modules\Tag\Contracts\TaggableInterface;
+use Modules\Tag\Repositories\TagManager;
+use Modules\Tag\Traits\NamespacedEntity;
+use Modules\Tag\Traits\TaggableTrait;
+
+class TagManagerRepositoryTest extends BaseTestCase
+{
+    /**
+     * @var TagManager
+     */
+    private $tagManager;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->tagManager = app(TagManager::class);
+    }
+
+    /** @test */
+    public function it_initialises_empty_array()
+    {
+        $this->assertEquals([], $this->tagManager->getNamespaces());
+    }
+
+    /** @test */
+    public function it_adds_items_to_array()
+    {
+        $this->tagManager->registerNamespace($this->getEntityMock());
+
+        $this->assertCount(1, $this->tagManager->getNamespaces());
+    }
+
+    private function getEntityMock()
+    {
+        return new class implements TaggableInterface {
+            use TaggableTrait, NamespacedEntity;
+            protected static $entityNamespace = 'asgardcms/media';
+        };
+    }
+}
+
