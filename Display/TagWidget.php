059d734f12bafc1d60e3d7e30e3305b76e920147
Nicolas Widart <n.widart@gmail.com>
2016-07-13 21:32:01 +0200

    Adding a way to get the current tags or tags from previous request

--- Display/TagWidget.php
+++ Display/TagWidget.php
@@ -29,6 +29,10 @@ class TagWidget
         $this->tag = $tag;
     }
 
+    /**
+     * @param $arguments
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show($arguments)
     {
         $this->extractArguments($arguments);
@@ -37,13 +41,31 @@ class TagWidget
 
         $availableTags = $this->tag->allForNamespace($this->namespace);
 
-        return view($view, compact('availableTags'));
+        $tags = $this->getTags();
+
+        return view($view, compact('availableTags', 'tags'));
     }
 
+    /**
+     * Extract the possible arguments as class properties
+     * @param array $arguments
+     */
     private function extractArguments(array $arguments)
     {
         $this->namespace = array_get($arguments, 0);
         $this->entity = array_get($arguments, 1);
         $this->view = array_get($arguments, 2);
+    }
+
+    /**
+     * Get the available tags, if an entity is available from that
+     * @return array
+     */
+    private function getTags()
+    {
+        if ($this->entity === null) {
+            request()->old('tags');
+        }
+        return request()->old('tags', $this->entity->tags->lists('slug')->toArray());
     }
 }
