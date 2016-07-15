45c277a4513a53a5d07cc6393f7921360812ae6d
Nicolas Widart <n.widart@gmail.com>
2016-07-15 16:17:20 +0200

    Load the page module and run its migrations

--- Tests/BaseTestCase.php
+++ Tests/BaseTestCase.php
@@ -2,20 +2,30 @@
 
 namespace Modules\Tag\Tests;
 
+use Illuminate\Contracts\Console\Kernel;
 use Illuminate\View\ViewServiceProvider;
 use Modules\Core\Providers\CoreServiceProvider;
+use Modules\Page\Providers\PageServiceProvider;
 use Modules\Tag\Providers\TagServiceProvider;
 use Nwidart\Modules\LaravelModulesServiceProvider;
 use Orchestra\Testbench\TestCase;
 
 abstract class BaseTestCase extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->resetDatabase();
+    }
+
     protected function getPackageProviders($app)
     {
         return [
             ViewServiceProvider::class,
             LaravelModulesServiceProvider::class,
             CoreServiceProvider::class,
+            PageServiceProvider::class,
             TagServiceProvider::class,
         ];
     }
@@ -30,5 +40,31 @@ abstract class BaseTestCase extends TestCase
             'prefix' => '',
         ));
         $app['config']->set('translatable.locales', ['en', 'fr']);
+        $app['config']->set('modules.paths.modules', __DIR__ . '/../Modules');
+    }
+
+    private function resetDatabase()
+    {
+        // Relative to the testbench app folder: vendors/orchestra/testbench/src/fixture
+        $migrationsPath = 'Database/Migrations';
+        $artisan = $this->app->make(Kernel::class);
+        // Makes sure the migrations table is created
+        $artisan->call('migrate', [
+            '--database' => 'sqlite',
+            '--path'     => $migrationsPath,
+        ]);
+        // We empty all tables
+        $artisan->call('migrate:reset', [
+            '--database' => 'sqlite',
+        ]);
+        // Migrate
+        $artisan->call('migrate', [
+            '--database' => 'sqlite',
+            '--path'     => $migrationsPath,
+        ]);
+        $artisan->call('migrate', [
+            '--database' => 'sqlite',
+            '--path'     => 'Modules/Page/Database/Migrations',
+        ]);
     }
 }
