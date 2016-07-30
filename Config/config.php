98636ebfb1160e3402679eba908440a28f2a1105
Nicolas Widart <n.widart@gmail.com>
2016-07-30 14:29:46 +0200

    Adding custom sidebar key

--- Config/config.php
+++ Config/config.php
@@ -2,4 +2,14 @@
 
 return [
     'name' => 'Tag',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Sidebar Class
+    |--------------------------------------------------------------------------
+    | If you want to customise the admin sidebar ordering or grouping
+    | You can define your own sidebar class for this module.
+    | No custom sidebar: null
+    */
+    'custom-sidebar' => null,
 ];
